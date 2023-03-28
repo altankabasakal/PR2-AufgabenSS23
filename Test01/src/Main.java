50d3c8d0a79bdda9d0e9a0af4586b58fc0f32a78
altan <altan@141.19.150.9>
2023-03-28 10:09:04 +0200

    commit

--- Test01/src/Main.java
+++ Test01/src/Main.java
@@ -1,36 +1,17 @@
-/*
- * 
- 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
 
-public class Main {
-
-	public static void main(String[] args) throws FileNotFoundException {
-
-		Scanner s = new Scanner(System.in);
-
-		String path = "\"C:\\Users\\noorm\\pr1.3\\PR2\\src\\ÜB2\\dat\"";
-
-		File datei = new File(path);
-		if (datei.exists()) {
-			try {
-				FileReader eing = new FileReader(datei);
-				int c;
-				while ((c = eing.read()) != -1)
-					System.out.println((char) c);
-			} catch (IOException ausnahme) {
-				System.out.println("fehler " + ausnahme);
-			}
-		}
-
+public class Main{
+	
+	private String firmenName;
+	private String adresse;
+	private String bank;
+	
+	private static String cfgFile = "myconf.cfg";
+	
+	public static void main(String[] args){
+		
+		
+		
 	}
+	
 }
-
-
-}
-*/
-
