e5fe9405cd82173e413bcf70da823ef038fa5721
altan <altan@141.19.150.9>
2023-03-28 08:53:23 +0200

    first

---
+++ Test01/src/TestI.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+
+public class TestI {
+
+	public static void main(String[] args) {
+		
+	
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Bitte gebe deinen Namen an.");
+		String name = scan.nextLine();
+		
+		System.out.println(name);
+		
+		
+		
+
+	}
+
+}
